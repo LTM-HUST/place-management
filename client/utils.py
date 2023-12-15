c4e924d4a5be6719ac7683d17a0e3311cca01441
huydt84 <viethuy061002@gmail.com>
2023-12-15 13:09:32 +0700

    add send_friend_task wrapper

--- client/utils.py
+++ client/utils.py
@@ -2,6 +2,9 @@ import socket
 from _thread import *
 import threading
 
+from typing import Literal
+import json
+
 def recvall_str(sock):
     # Helper function to recv all streaming data using \r\n
     data = ""
@@ -19,3 +22,34 @@ def recvall_str(sock):
 def sendall_str(sock, message):
     message += "\r\n"
     sock.sendall(message.encode("utf-8"))
+    
+def send_friend_task(sock,
+                     task: Literal["send_friend_request", "accept_friend_request", "reject_friend_request",
+                                   "delete_friend", "view_friend_list", "view_friend_request"],
+                     friend_id: int = None):
+    if task in ["send_friend_request", "accept_friend_request", "reject_friend_request", "delete_friend"] and friend_id is None:
+        raise ValueError("friend_id is required!")
+    
+    if task == "send_friend_request":
+        content = {
+            "target_friend_id": friend_id
+        }
+    elif task in ["accept_friend_request", "reject_friend_request"]:
+        content = {
+            "source_friend_id": friend_id
+        }
+    elif task == "delete_friend":
+        content = {
+            "friend_id": friend_id
+        }
+    else:
+        content = {}
+        
+    message_json = {
+        "task": task,
+        "content": content
+    }
+    
+    message = json.dumps(message_json)
+    sendall_str(sock, message)
+        
