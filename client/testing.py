bac2a206b07bf513394a875b59d6d86bfaff1444
hieu748159263 <hieu748159263@gmail.com>
2024-01-12 08:20:11 +0700

    send msg using sock function, not wrapper function

--- client/testing.py
+++ client/testing.py
@@ -12,16 +12,23 @@ def main():
     first_resp = recvall_str(sock)
     session_id = first_resp['session_id']
 
-    with open('./client/testing_send.txt', 'r') as file:
-        for line in file:
-            try:
-                message = json.loads(line)
-                message['session_id'] = session_id
-            except json.decoder.JSONDecodeError:
-                message = line
+    # with open('./client/testing_send.txt', 'r') as file:
+    #     for line in file:
+    #         try:
+    #             message = json.loads(line)
+    #             message['session_id'] = session_id
+    #         except json.decoder.JSONDecodeError:
+    #             message = line
 
-            sendall_str(sock, message)
-            print(recvall_str(sock))
+    #         sendall_str(sock, message)
+    #         print(recvall_str(sock))
+
+    message = '{"session_id": "", "task": "register", "content": {"username": "huy", "password": "123456", "retype_password": "111111"}}\r\n{"session_id": "", "task": "register", "content": {"username": "huy", "password": "", "retype_password": "123456"}}\r\n'
+    sock.sendall(message.encode("utf-8"))
+    packet = sock.recv(1024)
+    print(packet)
+    packet = sock.recv(1024)
+    print(packet)
 
     sock.close()
 
